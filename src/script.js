625a316dc6f40df29a6a40b9c531c07cfdc31bf0
pandawan69 <luissimas@protonmail.com>
2020-11-26 11:04:24 -0300

    adding cities list to autocomplete

--- src/script.js
+++ src/script.js
@@ -1,64 +1,125 @@
 // API info
 const apiKey = '3b6c414b301c5501f7cfe3b433d89d7f';
 
-
-// Setting default URL for the api
+// Setting up axios instances
 //api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
-axios.defaults.baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+const axiosAPI = axios.create({
+  baseURL: 'https://api.openweathermap.org/data/2.5/weather'
+})
+const axiosLocal = axios.create({
+  baseURL: 'http://localhost:8080/cities'
+})
 
 // DOM elements
 let inputText = document.querySelector('#form input');
 let btnQuery = document.querySelector('#form button');
-let displayDiv = document.querySelector('#display');
+let divSugestion = document.querySelector('#displaySugestion')
+let divWeather = document.querySelector('#displayWeather');
 
 // Global variables
-let citiesArray = [];
 let forecast;
 
-//TODO fix with openweather parameters
-function getCity(queryText) {
-  axios.get(autocompleteURL, {params: {apikey: apiKey, q: queryText, language: 'pt-BR'}})
-    .then(function (response) {
-      citiesArray = response;
-    }).catch(function (error) {
+
+function getCities() {
+  axiosLocal.get('city.list.json')
+    .then((response) => {
+      autocomplete(response.data);
+    }).catch((error) => {
       console.log(error);
     });
 }
 
-function getForecast(queryText) {
-  axios.get('', {params: {q: queryText, appid: apiKey, units: 'metric', lang: 'pt_br'}})
-    .then(function (response) {
-      forecast = response.data;
-    }).catch(function (error) {
+// Requisição assíncrona para a API
+function getForecast() {
+  axiosAPI.get('', {params: {id: inputText.dataset.id, appid: apiKey, units: 'metric', lang: 'pt_br'}})
+    .then((response) => {
+      console.log(response.data);
+      // Mostra o resultado para o usuário
+      displayForecast(response.data);
+    }).catch((error) => {
       console.log(error);
     });
 }
 
-function autocomplete() {
-  getCity(inputText.value);
+function displayForecast(forecast) {
+  divWeather.innerHTML = '';
 
-  let cityName = citiesArray.data[0].LocalizedName;
-  let cityUF = citiesArray.data[0].AdministrativeArea.ID;
-
-  console.log(cityName + ' - ' + cityUF);
-}
-
-function displayForecast() {
   let titleElement = document.createElement('h2');
   let descriptionElement = document.createElement('h3');
   let temperatureElement = document.createElement('h3');
+  let timeElement = document.createElement('h3');
 
-  getForecast(inputText.value);
+  // Gerando as datas meu deus do céu não é possível que só dê para fazer desse
+  // jeito cara, que bagulho horrível. Tô pegando o offset da data local, somando
+  // de volta na data local pra gerar uma data no fuso horário padrão, daí eu uso 
+  // essa data e coloco o offset da cidade que a API retornou, e finalmente encontro
+  // o horário no fuso horário da cidade. As multiplicações são as conversões de tudo
+  // pra milisegundos
+  let localDate = new Date();
+  let dateOffset = localDate.getTimezoneOffset();
+  let utcDate = new Date(Date.now() + (dateOffset * 60 * 1000));
+  let cityDate = new Date(utcDate.getTime() + (forecast.timezone * 1000));
 
-  console.log(forecast.weather[0].main);
+  let dateString = cityDate.getUTCDate() + '/' + (cityDate.getUTCMonth() + 1) + '/' + cityDate.getUTCFullYear();
+  let timeString = cityDate.getHours() + ':' + cityDate.getMinutes();
 
-  titleElement.appendChild(document.createTextNode('Weather in ' + forecast.name));
-  descriptionElement.appendChild(document.createTextNode(forecast.weather[0].main));
-  temperatureElement.appendChild(document.createTextNode(forecast.main.temp + '°C'));
+  titleElement.appendChild(document.createTextNode(forecast.name));
+  descriptionElement.appendChild(document.createTextNode(capitalize(forecast.weather[0].description)));
+  temperatureElement.appendChild(document.createTextNode(Math.round(forecast.main.temp) + '°C'));
+  timeElement.appendChild(document.createTextNode(dateString + '     ' + timeString));
 
-  displayDiv.appendChild(titleElement);
-  displayDiv.appendChild(descriptionElement);
-  displayDiv.appendChild(temperatureElement);
+  divWeather.appendChild(titleElement);
+  divWeather.appendChild(descriptionElement);
+  divWeather.appendChild(temperatureElement);
+  divWeather.appendChild(timeElement);
 }
 
-btnQuery.onclick = displayForecast;
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function autocomplete(cities) {
+  divSugestion.innerHTML = '';
+
+  let sugestions = cities.filter((cities) => {
+    return cities.name.toLowerCase().startsWith(inputText.value.toLowerCase().trim());
+  });
+
+  for (const city of sugestions) {
+    displaySugestion(city);
+  }
+}
+
+function displaySugestion(city) {
+  let cityElement = document.createElement('a');
+  cityElement.setAttribute('class', 'dropdown-item');
+  cityElement.setAttribute('href', '#');
+  cityElement.setAttribute('data-id', city.id);
+  cityElement.setAttribute('onclick', 'fillInput(this)')
+
+  cityElement.appendChild(document.createTextNode(city.name + ' - ' + city.country));
+
+  divSugestion.appendChild(cityElement);
+}
+
+function fillInput(element) {
+  inputText.setAttribute('data-id', element.dataset.id);
+  inputText.value = element.innerHTML.split(" - ")[0].trim();
+  divSugestion.innerHTML = '';
+
+  btnQuery.click();
+
+}
+
+
+inputText.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    btnQuery.click();
+  } else if (inputText.value === '') {
+    divSugestion.innerHTML = '';
+  } else {
+    getCities();
+  }
+});
+
+btnQuery.addEventListener('click', getForecast);
